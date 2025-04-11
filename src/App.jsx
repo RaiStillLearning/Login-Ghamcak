6e76b03b1019d4e4a1ca62514d1c2947fa4b8ec1
Xarch <rakhaarkhana98@gmail.com>
2025-04-11 19:56:22 +0700

    login pake auth0

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <button
+          className="btn btn-primary"
+          onClick={() => alert("Hello World!")}
+        >
+          Klik Button
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
